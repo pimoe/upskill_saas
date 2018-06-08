6922554cf7c9fc8c75ab7b712fb464fde8bc9cfd
pimoe <sheik.yo-ass@web.de>
2018-06-08 12:18:35 +0000

    new contact_mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class ContactMailer < ActionMailer::Base
   default to: 'test@test.com'
   
@@ -10,6 +9,3 @@ class ContactMailer < ActionMailer::Base
   
   mail(from: email, subject: 'Contact Form Message')
 end
-=======
-
->>>>>>> 0c212ad2907803874f13405f526e3fc7e10d3c0f
